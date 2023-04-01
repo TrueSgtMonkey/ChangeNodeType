d9be19db872ba3b38ea9aeada4bc8842c646d850
Cody Thompson <47369629+TrueSgtMonkey@users.noreply.github.com>
2023-04-01 14:14:33 -0700

    start

--- addons/changetype/ChangeType.gd
+++ addons/changetype/ChangeType.gd
@@ -11,7 +11,7 @@ func _input(event):
 		if options == null:
 			options = load("res://Menu/Options.tscn").instantiate()
 			add_child(options)
-			options.get_node("CenterContainer/OptionButton").connect("item_selected", selectType)
+			options.get_node("CenterContainer/OptionButton").connect("item_selected", selectType, CONNECT_DEFERRED)
 
 func selectType(index : int):
 	if options == null:
@@ -23,11 +23,11 @@ func selectType(index : int):
 	for node in nodes:
 		match index:
 			0: # CharacterBody3D
-				if !(node is CharacterBody3D):
-					Utils.changeType(node, CharacterBody3D.new())
-			1: # PhysicsBody3D
-				if !(node is RigidBody3D):
-					Utils.changeType(node, RigidBody3D.new())
+				print("Change ", node, " to CharacterBody3D -- ", index)
+			1: # RigidBody3D
+				print("Change ", node, " to RigidBody3D -- ", index)
+			_: # Cancel
+				print("Cancelled change type with: ", index)
 	
 	nodes.clear()
 	options.free()
@@ -36,7 +36,6 @@ func selectType(index : int):
 func _enter_tree():
 	# Initialization of the plugin goes here.
 	pass
-
 
 func _exit_tree():
 	# Clean-up of the plugin goes here.
